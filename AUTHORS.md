faa94efa37f8b61de4f5c76054d64f0509d86471
Jeff Tanner <jefft@tune.com>
2016-11-17 02:44:59 -0800

    [1] v 0.1.0 -- Initial Code

---
+++ AUTHORS.md
@@ -0,0 +1,17 @@
+## pyhttpstatus-utils Authors
+## Countries conversion functions for Python 2.7 and 3.0
+===
+
+## pyhttpstatus-utils Authors
+==========================================
+
+We'd like to thank the following people who have contributed to the `pyhttpstatus-utils` repository.
+
+- Jeff Tanner <jefft@tune.com>
+- Zaq? Wiedmann <zaq@tune.com>
+
+## pyhttpstatus-utils Maintainers
+==========================================
+
+- Jeff Tanner <jefft@tune.com>
+- Zaq? Wiedmann <zaq@tune.com>
---
+++ examples/example_http_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2016 TUNE, Inc. (http://www.tune.com)
+
+from collections import defaultdict
+
+from pprintpp import pprint
+
+from pyhttpstatus_utils import http_status_dict
+
+_http_status_dict = defaultdict(
+    lambda: 'partner returned non-200 status code',
+    http_status_dict({
+        401: 'Invalid api_key'
+    })
+)
+
+pprint(_http_status_dict[200])
+pprint(_http_status_dict[502])
+pprint(_http_status_dict[401])
+pprint(_http_status_dict[600])
+
+
+
